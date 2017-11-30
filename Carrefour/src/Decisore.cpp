9450cad3e8132f9b6a80e6321d136a574313a3d8
elescars <scarsellielena@gmail.com>
2017-11-30 10:55:06 +0100

    Decisore
    Creato .cc .h

---
+++ Carrefour/src/Decisore.cpp
@@ -0,0 +1,18 @@
+/*
+ * Decisore.cpp
+ *
+ *  Created on: 30 nov 2017
+ *      Author: elena
+ */
+
+#include "Decisore.h"
+
+Decisore::Decisore() {
+    // TODO Auto-generated constructor stub
+
+}
+
+Decisore::~Decisore() {
+    // TODO Auto-generated destructor stub
+}
+
---
+++ Carrefour/src/Decisore.h
@@ -0,0 +1,18 @@
+/*
+ * Decisore.h
+ *
+ *  Created on: 30 nov 2017
+ *      Author: elena
+ */
+
+#ifndef DECISORE_H_
+#define DECISORE_H_
+
+int numerocasse = 5;
+class Decisore {
+public:
+    Decisore();
+    virtual ~Decisore();
+};
+
+#endif /* DECISORE_H_ */
