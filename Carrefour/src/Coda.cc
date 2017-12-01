de1f26ecd6106c77c14b72e94d818e85f79f23b6
elescars <scarsellielena@gmail.com>
2017-12-01 16:47:51 +0100

    Coda.cc
    Bozza di gestione messaggi in coda

--- Carrefour/src/Coda.cc
+++ Carrefour/src/Coda.cc
@@ -14,14 +14,24 @@
 // 
 //PROVA
 #include "Coda.h"
-
+#include <omnetpp.h>
 Define_Module(Coda);
 
 void Coda::initialize()
 {
 }
 
-void Coda::handleMessage(cMessage *msg)
-{
-
+void Coda::handleMessage(cMessage *msg) {
+    QueueMessage* message = check_and_cast<QueueMessage*>(msg);
+        if (message->getOperationType() == NEW_CUSTOMER) {
+            queue.insert(message);
+        } else if (message->getOperationType() == CASSA_LIBERA) {
+            if (!queue.isEmpty()) {
+                QueueMessage* reply = check_and_cast<QueueMessage*>(queue.pop());
+                send(reply, msg->getSenderGate());
+            } else {
+                //Rimuovere dalla coda il primo entrato e mandarlo alla cassa
+            }
+        }
 }
+
