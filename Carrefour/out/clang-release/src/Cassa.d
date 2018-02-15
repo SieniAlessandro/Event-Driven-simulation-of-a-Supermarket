ef98b47f70598acc3715a857d838b5d0d4ac58ad
maghero <34124449+maghero@users.noreply.github.com>
2018-02-15 13:08:04 +0100

    Nessuna modifica

--- Carrefour/out/clang-release/src/Cassa.d
+++ Carrefour/out/clang-release/src/Cassa.d
@@ -1,10 +1,10 @@
 ../out/clang-release/src/Cassa.o: Cassa.cc Cassa.h \
-  C:/omnetpp-5.2.1-src-windows/omnetpp-5.2.1/include\omnetpp.h \
+  C:/Users/maghe/Downloads/omnetpp-5.2-src-windows/omnetpp-5.2/include\omnetpp.h \
   Decisore.h Customer.h
 
 Cassa.h:
 
-C:/omnetpp-5.2.1-src-windows/omnetpp-5.2.1/include\omnetpp.h:
+C:/Users/maghe/Downloads/omnetpp-5.2-src-windows/omnetpp-5.2/include\omnetpp.h:
 
 Decisore.h:
 
